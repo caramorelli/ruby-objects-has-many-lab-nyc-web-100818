1e5a266b8691b86d709b4977e2e0ed106a87e146
Cara Morelli <caramorelli7@gmail.com>
2018-10-11 11:17:01 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,16 +1,16 @@
 class Artist
   attr_accessor :name
 
-  @@song_count = 0
+  @@post_count = 0
 
   def initialize(name)
     @name = name
-    @songs = []
+    @posts = []
   end
 
   def add_post(post)
     self.posts << post
-    post.artist = self
+    post.author = self
     @@song_count +=1
   end
 
