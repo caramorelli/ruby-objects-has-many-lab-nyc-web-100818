651933c05f24f79a51307336c7164f21bd4086d1
Cara Morelli <caramorelli7@gmail.com>
2018-10-11 11:21:06 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,7 +16,7 @@ class Author
   end
 
   def add_post_by_title(title)
-     posts = Post.new(title)
+     post = Post.new(title)
     self.posts << posts
     post.author = self
     @@post_count +=1
