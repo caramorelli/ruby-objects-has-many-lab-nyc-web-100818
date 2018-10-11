6af610918ca19f577a18263e06983213dc974c93
Cara Morelli <caramorelli7@gmail.com>
2018-10-11 11:22:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,3 @@
-
-
 class Song 
   
   attr_accessor :artist, :all
@@ -27,4 +25,18 @@ class Song
     @@all
   end 
 end 
+
+
+class Song 
+  attr_accessor :name, :artist 
+
+  @@all = []
+
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+
 
