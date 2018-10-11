0f8e831c96160a5160870559b6d42004811622c9
Cara Morelli <caramorelli7@gmail.com>
2018-10-11 11:15:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,31 @@
+class Artist
+  attr_accessor :name
 
+  @@song_count = 0
 
-class Artist 
-  
-  attr_accessor :name, :all
-  
   def initialize(name)
     @name = name
-  end 
-  
-  def name 
-    @name
-  end 
-  
-  def songs 
-    Song.all
-  end 
-  
-  def add_song(song_name)
-    song_name.artist = self 
-  end 
-  
-  def add_song_by_name(song_name)
-  end 
-  
-  def self.song_count 
-    @songs.count
-  end 
-  
-end 
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def songs
+    @songs 
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
